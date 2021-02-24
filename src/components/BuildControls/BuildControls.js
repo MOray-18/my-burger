df2b6bf39f43fd011f98890e16b69ac994647f9f
Moray <mrnabar@gmail.com>
2021-02-24 16:17:10 +0530

    create an array of controls

---
+++ src/components/BuildControls/BuildControls.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = props => {
+  return <div></div>;
+};
+
+export default buildControls;
