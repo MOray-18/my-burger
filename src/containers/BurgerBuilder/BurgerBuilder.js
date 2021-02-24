643a150aa793b231837ca7e23bd29f72e7b38e94
Moray <mrnabar@gmail.com>
2021-02-24 16:36:33 +0530

    add pricings for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/BuildControls/BuildControls';
 
+const INGREDIENT_PRICE = {
+  salad: 5,
+  bacon: 15,
+  cheese: 5,
+  meat: 12,
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -12,14 +19,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
     },
+    totalPrice: 25,
   };
 
   addIngredientHandler(type) {
     const prevCount = this.state.ingredients[type];
     const updatedIngs = { ...this.state.ingredients };
+    const prevPrice = this.state.totalPrice;
+    const additionalPrice = INGREDIENT_PRICE[type];
 
     updatedIngs[type] = prevCount + 1;
-    this.setState({ ingredients: updatedIngs });
+    const updatedPrice = prevPrice + additionalPrice;
+
+    this.setState({ ingredients: updatedIngs, totalPrice: updatedPrice });
   }
 
   removeIngredientHandler(type) {}
