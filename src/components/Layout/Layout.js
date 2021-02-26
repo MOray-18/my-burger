4585ce138be4a16e1391baf4ac91c7798da3b4de
Moray <mrnabar@gmail.com>
2021-02-26 20:18:30 +0530

    add toggle handler to control side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,20 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+  sideDrawerToggleHandler = () =>
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+
+  sideDrawerClosedHandler = () => this.setState({ showSideDrawer: false });
 
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar openDrawer={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
