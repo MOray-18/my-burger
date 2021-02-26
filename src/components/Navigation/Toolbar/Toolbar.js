5444c3bd7fe3421e8118df21d9a91a991dfe6551
Moray <mrnabar@gmail.com>
2021-02-26 20:20:38 +0530

    add toggle component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import ToggleDrawer from '../SideDrawer/ToggleDrawer/ToggleDrawer';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <ToggleDrawer clicked={props.openDrawer} />
     <div className={classes.Logo}>
       <Logo />
     </div>
