7a68f0938efded12a0159718c0fde7e38dc633b8
Moray <mrnabar@gmail.com>
2021-02-27 18:23:51 +0530

    edit checkout handler to send data to server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICE = {
   salad: 5,
@@ -38,7 +39,28 @@ class BurgerBuilder extends Component {
 
   purchaseCancelHandler = () => this.setState({ isPurchasing: false });
 
-  purchaseContinueHandler = () => alert('You Continue!');
+  purchaseContinueHandler = () => {
+    // alert('You Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Moreshwar Nabar',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '400610',
+          country: 'India',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+
+    axios
+      .post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
 
   addIngredientHandler(type) {
     const prevCount = this.state.ingredients[type];
